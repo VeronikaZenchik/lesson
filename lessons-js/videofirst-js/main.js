9edc9f07688bfe86b7850ededa21777f7f9d8031
VeronikaZenchik <veronikazenchik991@gmail.com>
2024-07-22 13:35:28 +0300

    first lesson

---
+++ lessons-js/videofirst-js/main.js
@@ -0,0 +1,20 @@
+const INITIAL_ENERGY = 10;
+const MIN_ENERGY_LEVEL = 0;
+const LOW_ENERGY_THRESHOLD = 3;
+
+
+let energy = INITIAL_ENERGY;
+
+while (energy >= MIN_ENERGY_LEVEL) {
+  console.log("Energy " + energy);
+  if (energy === MIN_ENERGY_LEVEL) {
+    console.log("Нужна подзарядка");
+  } else if(energy <= LOW_ENERGY_THRESHOLD) {
+    console.log("Силы заканчиваются!");
+  } else {
+    console.log("Работаем дальше!");
+  }
+  energy = energy - 1;
+  }
+
+  console.log("Робот отдыхает");
