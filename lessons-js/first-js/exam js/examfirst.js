529e409ab2ed556cdfbfbc001862ecdc538a24a8
VeronikaZenchik <veronikazenchik991@gmail.com>
2024-07-26 18:12:08 +0300

    examfirst

---
+++ lessons-js/first-js/exam js/examfirst.js
@@ -0,0 +1,28 @@
+// ЭКЗАМЕН ПЕРВЫЙ ПО js
+
+// let y = 10
+// if (true) {
+//   let y = 100
+//   console.log(y)
+// }
+// console.log(y)
+
+
+//  var
+// var y = 10
+// if (true) {
+//   var y = 100
+//   console.log(y)
+// }
+// console.log(y)
+
+
+
+// Перепиши выражение, результат которого присваивается message, используя шаблонную строку. 
+
+// В ответе укажи эту шаблонную строку. Пример ответа: `Hello!`
+
+
+// let energy = 5
+// let name = "Robo"
+// let message = name + " имеет " + energy + " единиц энергии"
