15d738536ff4ba4889f284339604e35a7ea12bf7
VeronikaZenchik <veronikazenchik991@gmail.com>
2024-07-28 17:53:01 +0300

    switch commit

--- lessons-js/lesson 2/videosecond-js/main.js
+++ lessons-js/lesson 2/videosecond-js/main.js
@@ -1,3 +1,6 @@
+// Делаем нашу игру с помощью IF/ELSE
+
+
 let userChoice;             //для хранения выбора пользователя
 let computerChoice;         //для хранения выбора компьютера
 let isWinner = false;       // поможет нам определить, когда игра должна закончиться
@@ -17,6 +20,18 @@ while (!isWinner) {
     computerChoice = "бумага"
   }
 
+// switch (randomNumber) { //заменяем на switch с IF  
+//   case 0:
+//     computerChoice = "камень"
+//     break;
+//   case 1:
+//     computerChoice = "ножницы"
+//     break;
+//   case 2:
+//     computerChoice = "бумага"
+//     break;
+// }
+
   if (userChoice === "камень" || 
       userChoice === "ножницы" || 
       userChoice === "бумага") {
@@ -25,19 +40,32 @@ while (!isWinner) {
 
     if (computerChoice === userChoice) {
       alert("Ничья, давай ещё разок!")
-    } else if ((userChoice === "камень" &&  computerChoice === "ножницы" ||  
-              userChoice === "ножницы" && computerChoice === "бумага" ||
-              userChoice === "бумага" && computerChoice === "камень")) { //когда выйграл user
+    } else if ((userChoice === "камень" &&  computerChoice === "ножницы") ||  
+              (userChoice === "ножницы" && computerChoice === "бумага") ||
+              (userChoice === "бумага" && computerChoice === "камень")) { //когда выйграл user
       alert("Ты выиграл!")
       isWinner === true
     } else { // победа компа
       alert("Ты проиграл!")
       isWinner === true
+
+      // //Тернарный оператор
+      // if (computerChoice === userChoice) {
+      //   alert("Ничья, давай ещё разок!")
+      // } else {
+      //   const isUserWinner =
+      //       (userChoice === "камень" &&  computerChoice === "ножницы") ||  
+      //       (userChoice === "ножницы" && computerChoice === "бумага") ||
+      //       (userChoice === "бумага" && computerChoice === "камень")
+      
+      //   const message = isUserWinner ? "Ты выиграл!" : "Ты проиграл!";
+      //   alert(message)
+      //   isWinner === true
+      // }
+      
     } 
   } else {
     alert("Введите правильное значение: камень, ножницы или бумага")
   }
   
 }
-
-
