b687c00bb71672688574326a6a1c03053a7badf6
VeronikaZenchik <veronikazenchik991@gmail.com>
2024-07-30 22:48:19 +0300

    lesson3

--- lessons-js/lesson 3/thirdlesson-js/main.js
+++ lessons-js/lesson 3/thirdlesson-js/main.js
@@ -1,3 +1,5 @@
+// FUNCTION
+
 // let dogName = "rover";
 // let dogWeight = 23;
 // if (dogWeight > 20) {
@@ -42,3 +44,49 @@ bark("rover", 23)
 bark("spot", 13)
 bark("spike", 53)
 bark("lady", 17)
+
+
+//FUNCTION RETURN
+
+// deposit
+// const depositAmount = 1000;
+// const yearlyRate = 15;
+// const depositTermInYears = 2;
+
+// let depositIncome = 0;
+// let count = 0;
+
+// while (count < depositTermInYears) {
+//   depositIncome = depositIncome + depositAmount * (yearlyRate / 100);
+//   count = count + 1
+// }
+
+// console.log(depositIncome);
+
+
+//делаем из вышеуказанного  кода с помощью фунции (это если у нас будет несколько значений)
+
+const depositAmount = 1000;
+const yearlyRate = 15;
+const depositTermInYears = 2;
+
+
+
+function getDepositIncome(amount, rate, term) {
+  let count = 0;  
+  let depositIncome = 0;
+
+    while (count < term) {
+    depositIncome = depositIncome + amount * (rate / 100);
+    count = count + 1
+  }
+  return depositIncome;
+}
+
+const depositIncome_1 = getDepositIncome(depositAmount, yearlyRate, depositTermInYears);
+const depositIncome_2 = getDepositIncome(1000, 30, 3);
+const totalIncome = depositIncome_1 + depositIncome_2;
+
+console.log(totalIncome);
+
+// console.log(depositIncome_2)
