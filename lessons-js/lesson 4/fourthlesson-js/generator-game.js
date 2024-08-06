0902c28315ea51e596dbe9643e7e3e7dba8fcce6
VeronikaZenchik <veronikazenchik991@gmail.com>
2024-08-06 22:16:30 +0300

    finish game

--- lessons-js/lesson 4/fourthlesson-js/generator-game.js
+++ lessons-js/lesson 4/fourthlesson-js/generator-game.js
@@ -1,17 +1,29 @@
 
-
-function makePrases() {
-  const words1 = ["Мой кот", "Моя собака", "Мой попугай"];
-  const words2 = ["любит есть", "хочет погрызать", "всегда ищет"];
-  const words3 = ["морковку", "макароны", "косточку"];
-
-  const randomIndex_1 = Math.floor(Math.random() * words1.length);
-  const randomIndex_2 = Math.floor(Math.random() * words1.length);
-  const randomIndex_3 = Math.floor(Math.random() * words1.length);
-
-  const phrase = words1[randomIndex_1] + " " + words2[randomIndex_2] + " " + words3[randomIndex_3];
-
-  alert(phrase);
+function getRandomElement(array) {
+  const randomIndex = Math.floor(Math.random() * array.length);
+  return array [randomIndex]
 }
 
-makePrases();
+
+const words1 = ["Мой кот", "Моя собака", "Мой попугай"];
+const words2 = ["любит есть", "хочет погрызать", "всегда ищет"];
+const words3 = ["морковку", "макароны", "косточку"];
+const words4 = ["Мой хомяк", "Моя крыса", "Мой енот"];
+
+function makePrases(param1, param2, param3) {
+
+  // const randomIndex_1 = Math.floor(Math.random() * words1.length);
+  // const randomIndex_2 = Math.floor(Math.random() * words1.length);
+  // const randomIndex_3 = Math.floor(Math.random() * words1.length);
+  // const phrase = words1[randomIndex_1] + " " + words2[randomIndex_2] + " " + words3[randomIndex_3];
+
+  const result = getRandomElement(param1) + " " + getRandomElement(param2) + " " + getRandomElement(param3);
+  return result
+}
+
+// не создаем лишних переменных
+alert(makePrases(words4, words2, words3)); 
+
+// так пишем если хотим где-то еще использовать эту фразу
+// const result = makePrases();
+// console.log(result);
