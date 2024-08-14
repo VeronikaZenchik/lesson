14b487eaef6a72e2fbe2c1a3673fad04eed6c73b
VeronikaZenchik <veronikazenchik991@gmail.com>
2024-08-14 17:33:33 +0300

    new lesson 5

---
+++ lessons-js/exam js/JS part-1/totalexam-1.js
@@ -0,0 +1,6 @@
+let y = 10
+if (true) {
+  let y = 100
+  console.log(y)
+}
+console.log(y)
---
+++ lessons-js/lesson 5/fifthlesson-js/main.js
@@ -0,0 +1,36 @@
+// cars:
+
+//make: "Chevy",
+// model: "Bel Air",
+// year: 1957,
+// color: "red",
+// passengers: 2,
+// convertiblr: false,
+// mileage: 1021
+
+// dog:
+
+// name: "Fido",
+// weight: 20.2,
+// age: 4,
+// breed: "mixed",
+// activity: "fetch balls"
+
+
+const car = {
+  make: "Chevy",
+  model: "Bel Air",
+  year: 1957,
+  color: "red",
+  passengers: 2,
+  convertiblr: false,
+  mileage: 1021
+}
+
+const dog = {
+  name: "Fido",
+  weight: 20.2,
+  age: 4,
+  breed: "mixed",
+  activity: "fetch balls"
+  }
