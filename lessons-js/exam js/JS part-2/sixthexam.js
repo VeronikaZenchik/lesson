b7e085bc24e06408c3ab67d7af8f76bc8f4559a0
VeronikaZenchik <veronikazenchik991@gmail.com>
2024-08-25 18:21:09 +0300

    sixth exam

---
+++ lessons-js/exam js/JS part-2/sixthexam.js
@@ -0,0 +1,15 @@
+1. title.textContent
+
+2. setAttribute
+
+3. document.querySelectorAll('li')
+
+4. document.getElementById('rain-button')
+
+5. menuItem.classList.add('sctive')
+
+6. infoBox.style.backgroundColor
+
+7. infoText.classList.remove('info')
+
+8. box.innerHTML
